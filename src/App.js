91ec53e452f32747d5fccad1f330596b3753b916
Sanae <sanae.s.soma@gmail.com>
2023-09-06 20:21:52 -1000

    add MyList component in App.js

--- src/App.js
+++ src/App.js
@@ -57,12 +57,26 @@ function App() {
     )
   };
 
+  function MyList() {
+    return (
+      <div className="myList">
+      <h3>My List</h3>
+      <ul className="locationCard">
+        <li>Location Name</li>
+        <li>Location Address</li>
+        <li>Category: category</li>
+        <button>Get coupon</button>
+      </ul>
+      </div>
+    )
+  };
 
   return (
     <div className="App">
       <Header />
       <ListContainer />
       <AddNewLocation />
+      <MyList />
     </div>
   );
 }
