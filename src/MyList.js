d3326859f5a4d314a7fd576e18b080611c4cdfee
Sanae <sanae.s.soma@gmail.com>
2023-09-10 15:16:26 -1000

    modify conditional statement in MyList.js to render items in myList prop correctly

--- src/MyList.js
+++ src/MyList.js
@@ -7,7 +7,7 @@ function MyList({ myList }) {
     return (
       <div className="myList">
         <h3>My List</h3>
-        {myList === true
+        {myList.length > 0
           ? myList.map(location => <Card key={location.id} name={location.name} address={location.address} category={location.category} />)
           : <Link to="/">Add locations to your list!</Link>
         }
