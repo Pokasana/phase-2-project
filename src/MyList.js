1e6abd9965ebcb75c31fa286b0874d651efd90bb
Sanae <sanae.s.soma@gmail.com>
2023-09-10 14:34:42 -1000

    add Link hook to MyList.js to programmatically navigate the user to Home when there is no location in my list

--- src/MyList.js
+++ src/MyList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import { Link } from 'react-router-dom';
 
 function MyList({ myList }) {
 
@@ -7,7 +8,7 @@ function MyList({ myList }) {
       <div className="myList">
         <h3>My List</h3>
         {myList === true
-          ? myList.map(location => <Card key={location.id} />) : <p>Add locations to your list!</p>
+          ? myList.map(location => <Card key={location.id} />) : <Link to="/">Add locations to your list!</Link>
         }
       </div>
     )
