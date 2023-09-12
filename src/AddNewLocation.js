603d680a4a94ba3d8157344d8e760f15dfee61a9
Sanae <sanae.s.soma@gmail.com>
2023-09-12 00:00:05 -1000

    add name attribute to input tags in AddNewLocation.js

--- src/AddNewLocation.js
+++ src/AddNewLocation.js
@@ -7,17 +7,16 @@ function AddNewLocation() {
     category: ""
   });
 
-  console.log(formData);
 
     return (
       <div className="add-new">
         <form>
           <label htmlFor="name">Location Name:</label>
-          <input type="text" id="newName"></input><br></br>
+          <input type="text" id="newName" name="name"></input><br></br>
           <label htmlFor="address">Address:</label>
-          <input type="text" id="newAddress"></input><br></br>
+          <input type="text" id="newAddress" name="address"></input><br></br>
           <label htmlFor="category">Category:</label>
-          <input type="text" id="newCategory"></input><br></br>
+          <input type="text" id="newCategory" name="category"></input><br></br>
 
           <input type="submit" value="Submit"></input>
         </form>
