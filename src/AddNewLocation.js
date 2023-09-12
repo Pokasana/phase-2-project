92c7f31022b1023649846d2d5569a144c6c025c9
Sanae <sanae.s.soma@gmail.com>
2023-09-12 00:02:15 -1000

    add onChange to input tags in AddNewLocation.js

--- src/AddNewLocation.js
+++ src/AddNewLocation.js
@@ -7,16 +7,20 @@ function AddNewLocation() {
     category: ""
   });
 
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    console.log(e.target.name);
+  };
 
     return (
       <div className="add-new">
         <form>
           <label htmlFor="name">Location Name:</label>
-          <input type="text" id="newName" name="name"></input><br></br>
+          <input type="text" id="newName" name="name" onChange={handleInput}></input><br></br>
           <label htmlFor="address">Address:</label>
-          <input type="text" id="newAddress" name="address"></input><br></br>
+          <input type="text" id="newAddress" name="address" onChange={handleInput}></input><br></br>
           <label htmlFor="category">Category:</label>
-          <input type="text" id="newCategory" name="category"></input><br></br>
+          <input type="text" id="newCategory" name="category" onChange={handleInput}></input><br></br>
 
           <input type="submit" value="Submit"></input>
         </form>
